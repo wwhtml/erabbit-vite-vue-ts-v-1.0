ed102e646adf142dd0e39c3db4696b57413cca45
wwhtml <1129942397@qq.com>
2024-01-02 17:20:00 +0800

    aa

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,9 @@ module.exports = {
   },
   rules: {
     /* eslint */
-    "vue/multi-word-component-names": "off" //采用多词组件名称：禁止
+    "vue/multi-word-component-names": "off", //必须采用多词组件名称：关闭
+
+    /* typescript-eslint */
+    "@typescript-eslint/no-unused-vars": "off" //禁止声明未使用的变量  关闭
   }
 };
